edca37483de234ed4f3dd051252c92d42d24c83b
nabihbassil <54837347+nabihbassil@users.noreply.github.com>
2022-06-26 20:03:18 +0200

    Send data of type Fact to facts screen

--- swm_app/lib/services/fact_service.dart
+++ swm_app/lib/services/fact_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:swm_app/model/facts_model.dart';
 
 class FactService {
   final CollectionReference profileList =
@@ -19,5 +20,17 @@ class FactService {
       print("error ${e.toString()}");
       return null;
     }
+  }
+
+  Future<List<Facts>> getUserTaskList() async {
+    QuerySnapshot qShot =
+        await FirebaseFirestore.instance.collection('awafacts').get();
+
+    return qShot.docs
+        .map((doc) => Facts(
+            awatext: doc.get("awatext"),
+            awatitle: doc.get("awatitle"),
+            awaimg: doc.get("awaimg")))
+        .toList();
   }
 }
