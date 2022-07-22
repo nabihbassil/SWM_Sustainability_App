81e53a39565f0b48b0904113639ce9e0dbedb61a
Daniela Ortiz <danielaortiz.del@gmail.com>
2022-07-22 13:12:13 +0200

    Burger Menu - Include user profile picture

--- swm_app/lib/Components/hamburger_menu.dart
+++ swm_app/lib/Components/hamburger_menu.dart
@@ -5,6 +5,7 @@ import 'package:swm_app/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import '../screens/login_screen.dart';
+import 'package:swm_app/services/user_service.dart';
 
 class HamburgerMenu extends StatefulWidget {
   const HamburgerMenu({Key? key}) : super(key: key);
@@ -16,9 +17,20 @@ class HamburgerMenu extends StatefulWidget {
 class _HamburgerMenu extends State<HamburgerMenu> {
   User? user = FirebaseAuth.instance.currentUser;
   UserModel loggedInUser = UserModel();
+  UserModel userData = UserModel();
+
+  fetchUserData() async {
+    userData = await UserService().getUserData();
+
+    setState(() {
+      userData;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    fetchUserData();
     FirebaseFirestore.instance
         .collection("users")
         .doc(user?.uid)
@@ -35,8 +47,27 @@ class _HamburgerMenu extends State<HamburgerMenu> {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
+          const SizedBox(height: 40),
+          SizedBox(
+            width: 20.0,
+            height: 70.0,
+            child: CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              child: ClipOval(
+                child: Image.network(
+                  userData.imgURL ??
+                      "https://soccerpointeclaire.com/wp-content/uploads/2021/06/default-profile-pic-e1513291410505.jpg",
+                  width: 60,
+                  height: 60,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
           SizedBox(
             height: 90.0,
+            width: 90,
             child: DrawerHeader(
                 child: ListTile(
                   title: Text(
@@ -45,7 +76,7 @@ class _HamburgerMenu extends State<HamburgerMenu> {
                 ),
                 decoration: const BoxDecoration(color: Colors.blueAccent),
                 margin: EdgeInsets.zero,
-                padding: EdgeInsets.zero),
+                padding: EdgeInsets.all(2)),
           ),
           ListTile(
             title: const Text('Logout'),
